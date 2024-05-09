ae4fa303664100920c2f380da4ea5f5678a7ad5a
dinhnamuet <20021163@vnu.edu.vn>
2024-05-09 10:02:36 +0700

    regmap test

--- ds3231/ds3231.c
+++ ds3231/ds3231.c
@@ -6,6 +6,8 @@
 #include <linux/workqueue.h>
 #include <linux/ktime.h>
 #include <linux/time.h>
+#include <linux/regmap.h>
+#include <linux/device.h>
 #include "ds3231.h"
 struct ds3231 {
     struct i2c_client *client;
@@ -13,6 +15,7 @@ struct ds3231 {
 
     struct work_struct workq;
     struct timer_list timer;
+    struct regmap *regmap;
 };
 
 static u8 ds3231_write8(struct ds3231 *rtc, u8 reg, u8 value);
@@ -26,6 +29,22 @@ static u8 encodeBCD8(u8 value);
 static u16 encodeBCD16(u16 value);
 static u8 decodeBCD8(u8 value);
 static u16 decodeBCD16(u16 value);
+
+static const struct regmap_range ds3231_ranges[] = {
+	{ .range_min = 0x00, .range_max = 0x0F },
+};
+static const struct regmap_access_table ds3231_table = {
+	.yes_ranges = ds3231_ranges,
+	.n_yes_ranges = ARRAY_SIZE(ds3231_ranges),
+};
+static struct regmap_config ds3231_reg_conf = {
+	.reg_bits	= 8,
+	.val_bits	= 8,
+	.max_register	= DS3231_REG_STATUS,
+	.volatile_table	= &ds3231_table,
+	.cache_type	= REGCACHE_RBTREE,
+	.name		= "ds3231",
+};
 
 static void timer_handler(struct timer_list *tm)
 {
@@ -50,6 +69,9 @@ static int ds3231_probe(struct i2c_client *client)
     rtc = kzalloc(sizeof(*rtc), GFP_KERNEL);
     if(!rtc)
         return -ENOMEM;
+    rtc->regmap = devm_regmap_init_i2c(client, &ds3231_reg_conf);
+    if (IS_ERR(rtc->regmap))
+		return PTR_ERR(rtc->regmap);
     i2c_set_clientdata(client, rtc);
     rtc->client = client;
     ds3231_init(rtc);
@@ -107,19 +129,27 @@ static u16 decodeBCD16(u16 value)
 }
 static u8 ds3231_write8(struct ds3231 *rtc, u8 reg, u8 value)
 {
-    return i2c_smbus_write_byte_data(rtc->client, reg, value);
+    //return i2c_smbus_write_byte_data(rtc->client, reg, value);
+    return regmap_write(rtc->regmap, reg, value);
 }
 static u8 ds3231_write16(struct ds3231 *rtc, u8 reg, u16 value)
 {
-    return i2c_smbus_write_word_data(rtc->client, reg, value);
+    //return i2c_smbus_write_word_data(rtc->client, reg, value);
+    return regmap_noinc_write(rtc->regmap, reg, &value, 2);
 }
 static u8 ds3231_read8(struct ds3231 *rtc, u8 reg)
 {
-    return i2c_smbus_read_byte_data(rtc->client, reg);
+    //return i2c_smbus_read_byte_data(rtc->client, reg);
+    int rd;
+    regmap_read(rtc->regmap, reg, &rd);
+    return (u8)rd;
 }
 static u16 ds3231_read16(struct ds3231 *rtc, u8 reg)
 {
-    return i2c_smbus_read_word_data(rtc->client, reg);
+    //return i2c_smbus_read_word_data(rtc->client, reg);
+    u16 rd;
+    regmap_noinc_read(rtc->regmap, reg, &rd, 2);
+    return rd;
 }
 static void ds3231_update_real_time(struct ds3231 *rtc)
 {
@@ -189,5 +219,5 @@ module_i2c_driver(ds3231_drv);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("DinhNam <20021163@vnu.edu.vn>");
-MODULE_DESCRIPTION("I2C SMBUS Test for DS3231");
+MODULE_DESCRIPTION("I2C Regmap Test for DS3231");
 MODULE_VERSION("1.0");
